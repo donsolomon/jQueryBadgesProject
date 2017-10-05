b2a36d2863c9f0b2c510d713227ae84e73f80787
donsolomon <dsolomon@ebsco.com>
2017-10-05 14:23:08 -0500

    step 1

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,3 +3,12 @@ $(function() {
   // your code will go here
 
 });
+
+
+$.ajax({
+  url: 'https://www.codeschool.com/users/2178110.json',
+  dataType: 'jsonp',
+  success: function(response) {
+    // handle response
+  }
+});
